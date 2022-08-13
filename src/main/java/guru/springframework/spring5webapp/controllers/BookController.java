9e791ce1c93c10dd6e572d5e748f82f3226b94fe
IShadowmaNI <marian.agheorghiesei@gmail.com>
2022-08-13 23:36:46 +0300

    Creating author controller, author and book lists in HTML using thymeleaf.

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -18,8 +18,8 @@ public class BookController {
     @RequestMapping("/books")
     public String getBooks(Model model) {
 
-        model.addAttribute("book", bookRepository.findAll());
+        model.addAttribute("books", bookRepository.findAll());
 
-        return "books";
+        return "books/list";
     }
 }
